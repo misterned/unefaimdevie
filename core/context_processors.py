445031a50af430bfd733964b874690dd7e391040
misterned <7439682+misterned@users.noreply.github.com>
2026-04-30 06:35:16 +0200

    Modification menu

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,17 @@
+from .models import Post
 from .services import user_is_animateur
 
 
 def role_flags(request):
     user = request.user
-    return {"is_animateur": user_is_animateur(user)}
+    nav_categories = (
+        Post.objects.filter(status=Post.Status.PUBLISHED)
+        .exclude(category="")
+        .values_list("category", flat=True)
+        .distinct()
+        .order_by("category")
+    )
+    return {
+        "is_animateur": user_is_animateur(user),
+        "nav_categories": list(nav_categories),
+    }
