e9f3685499c438535b065d6f4cfa4cebb14e2d94
misterned <7439682+misterned@users.noreply.github.com>
2026-05-22 10:24:07 +0200

    logging mail2

--- croisicwebzine/settings.py
+++ croisicwebzine/settings.py
@@ -1,3 +1,19 @@
+# -------------------------------------------------------------
+# LOGGING DJANGO POUR AZURE/DEBUG
+# -------------------------------------------------------------
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
 # croisicwebzine/settings.py
 
 import os
